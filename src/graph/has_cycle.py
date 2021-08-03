35de687462418dce44feadf050cb76e80ae08728
jadiel <teofilojadiel@gmail.com>
2021-08-02 22:33:20 -0300

    add has_cycle algo

---
+++ src/graph/has_cycle.py
@@ -0,0 +1,43 @@
+################ Graph cycle detection ################
+from typing import Set, Optional
+
+import neat_graph
+
+
+def has_cycle(graph: neat_graph.Graph) -> bool:
+    visited: Set[int] = set()
+    for vertex in graph.vertices:
+        if vertex in visited:
+            continue
+        if has_cycle_helper(graph, vertex, visited):
+            return True
+    return False
+
+def has_cycle_helper(graph: neat_graph.Graph, 
+                               starting_vertex: int, visited: Set[int], 
+                               local_visited: Optional[Set[int]] = None) -> bool:
+    """ Returns True if the DFS from starting_vertex hits a repeated node """
+    local_visited = set() if not local_visited else local_visited
+    local_visited.add(starting_vertex)
+    visited.add(starting_vertex)
+    for neighbor in graph.vertices.get(starting_vertex, []):
+        if neighbor in local_visited:
+            return True
+        if neighbor in visited:
+            continue
+        if has_cycle_helper(graph, neighbor, visited, local_visited):
+            return True
+    return False
+
+
+if __name__ == '__main__':
+    graph: neat_graph.Graph = neat_graph.Graph()
+    graph.add_edge(1,2)
+    graph.add_edge(3,2)
+    graph.add_edge(3,5)
+    graph.add_edge(3,1)
+    graph.add_edge(3,1)
+    print(graph, end='\n')
+    import pdb;pdb.set_trace()
+    has_cycle(graph)
+
