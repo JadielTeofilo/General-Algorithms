094354c1acd0e787353e640e556ca5d12b9f2186
dev <jadiel@localhost.localdomain>
2021-08-20 17:14:53 -0300

    equation solver initial implementation

---
+++ src/stacks_queues/equation_solver.py
@@ -0,0 +1,25 @@
+"""
+
+Calculator: Given an arithmetic equation consisting of positive integers,
+ +, -, * and / (no parentheses). compute the result.
+EXAMPLE
+Input:
+ 2*3+5/6*3+15
+Output:
+ 23.5
+
+How is the input camming in, as a list or string
+list
+[2 * 3 + 5 / 6 * 3 + 15]
+
+2 * 3 ### 5 / 6 * 3 ### 15
+
+Operation 
+	left Operation, int
+	right Operation, int
+	operator
+
+
+
+"""
+
