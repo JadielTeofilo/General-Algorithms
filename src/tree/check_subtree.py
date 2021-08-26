04ab6b465a37428e89be8c67edcf9ebd87de8ce4
dev <jadiel@localhost.localdomain>
2021-08-26 16:36:51 -0300

    add new algos

---
+++ src/tree/check_subtree.py
@@ -0,0 +1,63 @@
+"""
+Check Subtree: Tl and T2 are two very large binary trees, with Tl much bigger than T2. Create an
+algorithm to determine if T2 is a subtree of Tl .
+A tree T2 is a subtree of T1 if there exists a node n in Tl such that the subtree of n is identical to T2 .
+That is, if you cut off the tree at node n, the two trees would be identical.
+
+In - first_root: Node, second_root: Node
+Out - bool
+
+
+			  2
+	2					2
+3		5			1		7
+
+
+	2
+3		5
+
+Are there duplicate values on the tree?
+Yes
+
+
+"""
+from typing import Optional, Iterator
+import dataclasses
+
+
+@dataclasses.dataclass
+class Node:
+	value: int
+	left: Optional['Node'] = None
+	right: Optional['Node'] = None
+
+
+def is_subtree(root: Node, subtree_root: Node) -> bool:
+	if not root or not subtree_root:
+		raise ValueError('Empty inputs')
+	for node in traverse_tree(root):
+		if trees_are_equal(node, subtree_root):
+			return True
+	return False
+
+
+def traverse_tree(node: Node) -> Iterator[Node]:
+	if not node:
+		return
+	yield from traverse_tree(node.left)
+	yield node
+	yield from traverse_tree(node.right)
+
+
+def trees_are_equal(first: Node, second: Node) -> bool:
+	if not first or not second:
+		return first == second
+	if first.value != second.value:
+		return False
+	return (trees_are_equal(first.left, second.left) and 
+			trees_are_equal(first.right, second.right))
+
+root = Node(2, Node(2, Node(2, Node(3, Node(1)), Node(5))), Node(2, Node(1, Node(7))))
+sub_tree = Node(2, Node(3), Node(5))
+
+import pdb;pdb.set_trace()
--- src/tree/weave.py
+++ src/tree/weave.py
@@ -8,48 +8,19 @@ arrays that could have led to this tree.
 5 2 3
 
 
+1
 
-
-
-O(N^3*2^N) Time complexity
-Desconsiderando a concatenação de lista
+2
 
 
 
 
 """
-from typing import List, Optional
-import dataclasses
-
-
-@dataclasses.dataclass
-class Node:
-	value: int
-	left: Optional['Node'] = None
-	right: Optional['Node'] = None
-
-
-def possible_sequences(root: Node) -> List[List[int]]:
-	if not root:
-		return [[]]
-	left_sequences: List[List[int]] = possible_sequences(root.left)
-	right_sequences: List[List[int]] = possible_sequences(root.right)
-	sequences: List[List[int]] = weave_sequences(left_sequences, right_sequences)
-	return [[root.value] + sequence for sequence in sequences]
-	
-
-def weave_sequences(left_sequences: List[List[int]], 
-					right_sequences: List[List[int]]) -> List[List[int]]:
-	""" Weaves all possible combinations between the two list of lists """
-	result: List[List[int]] = []
-	for left_sequence in left_sequences:
-		for right_sequence in right_sequences:
-			result.extend(weave(right_sequence, left_sequence))
-	return result
+from typing import List
 
 
 def weave(left: List[int], right: List[int]) -> List[List[int]]:
-	""" Finds all possible combinations of left and right 
+	""" Gives all possible combinations of left and right 
 		keeping the original order on left and right """
 	if not left or not right:
 		return [left] if left else [right]
