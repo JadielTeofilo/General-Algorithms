779bef4323664b2420b5b624d5f9df790666bd7d
dev <teofilojadiel@gmail.com>
2022-03-18 17:11:32 -0300

    add new problems

--- src/interviewbit/tree/tree_from_inorder_postorder.py
+++ src/interviewbit/tree/tree_from_inorder_postorder.py
@@ -27,51 +27,53 @@ Out - TreeNode
 
 """
 import collections
-from typing import List
+from typing import List, Optional, Dict
 
 
 class TreeNode:
-	def __init__(self, x):
-		self.val = x
-		self.left = None
-		self.right = None
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 
 Boundery = collections.namedtuple('Boundery', 'start end')
 
 
 class Solution:
-	# @param A : list of integers
-	# @param B : list of integers
-	# @return the root node in the tree
-	def buildTree(self, in_order: List[int], 
-                      post_order: List[int]) -> TreeNode:
-            return self.build_from_orders(
-                boundery_in=Boundery(0, len(in_order) - 1), in_order, 
-                boundery_post=Boundery(0, len(post_order) - 1), post_order
+    # @param A : list of integers
+    # @param B : list of integers
+    # @return the root node in the tree
+    def buildTree(self, in_order: List[int], 
+                    post_order: List[int]) -> Optional[TreeNode]:
+            self.indexes: Dict[int, int] = {
+                num: i for i, num in enumerate(in_order)
+            }
+            return self.build_from_order(
+                Boundery(0, len(in_order) - 1), in_order, 
+                Boundery(0, len(post_order) - 1), post_order
             )
-    
-        def build_from_order(
+
+    def build_from_order(
             self, boundery_in: Boundery, in_order: List[int],
             boundery_post: Boundery, post_order: List[int]
-        ) -> TreeNode:
-            if boundery_in.start > boundery_in.end:
-                return None
-            root_val: int = post_order[boundery_post.end]
-            root_index: int = bisect.bisect_left(in_order, root_val)
-            current: TreeNode = TreeNode(root_val)
-            current.left = self.build_from_order(
-                Boundery(boundery_in.start, root_index - 1), in_order, 
-                Boundery(boundery_post.start, boundery_post.end - 1), 
-                post_order
-            )
-            new_boundery_post_end: int = (boundery_post.end - 
-                                          (2 if current.left else 2))
-            current.right = self.build_from_order(
-                Boundery(root_index + 1, boundery_in.end), in_order, 
-                Boundery(), post_order
-            )
-            return current
-            
-            
-
+    ) -> Optional[TreeNode]:
+        if boundery_in.start > boundery_in.end:
+            return None
+        root_val: int = post_order[boundery_post.end]
+        root_index: Optional[int] = self.indexes.get(root_val, None)
+        if not root_index:
+            return None
+        current: TreeNode = TreeNode(root_val)
+        current.left = self.build_from_order(
+            Boundery(boundery_in.start, root_index - 1), in_order, 
+            Boundery(boundery_post.start, boundery_post.end - 1), 
+            post_order
+        )
+        new_boundery_post_end: int = (boundery_post.end - 
+                                    (2 if current.left else 2))
+        current.right = self.build_from_order(
+            Boundery(root_index + 1, boundery_in.end), in_order, 
+            Boundery(boundery_post.start, new_boundery_post_end), post_order
+        )
+        return current
